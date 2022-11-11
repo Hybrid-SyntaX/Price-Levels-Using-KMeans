f6e4e963a1071e3bab507e2f1de952b413de7619
Abbas Allahyari <abbas.allahyari.91@gmail.com>
2022-11-12 00:58:44 +0330

    add to comma sperated tuple

---
+++ LevelsFinder/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelsFinder.Extensions
+{
+    public static class Extensions
+    {
+        public static string ToCommaSeperatedTuples(this List<Level> value)
+        {
+            var results = new string[value.Count];
+            for (int i = 0; i < value.Count; i++)
+            {
+                results[i] = $"[{value[i].Min},{value[i].Max}]";
+
+            }
+
+            return $"[{string.Join(", ", results)}]";
+        }
+    }
+}
